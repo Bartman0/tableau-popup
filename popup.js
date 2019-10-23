d1b091047e65df4ee0ecf98408a9e6e79b3e0c89
Richard Kooijman <rkooijman@quicknet.nl>
2019-10-23 23:42:08 +0200

    added popup Tableau popup added iconset

--- popup.js
+++ popup.js
@@ -14,9 +14,6 @@ function setup() {
 
   console.log("extension popup setup: ", selection);
 
-  // call Tableau
-  var url = tableau.server + "/" + tableau.workbook;
-
   function gotSelection(data) {
     var p = document.getElementById("selection");
     if (!p) {
@@ -31,54 +28,6 @@ function setup() {
 
   gotSelection(selection);
 
-  function getImage() {
-    jQuery.ajax({
-      url: url,
-      cache: false,
-      withCredentials: true,
-      xhr: function() {
-        var xhr = new XMLHttpRequest();
-        xhr.responseType = "blob";
-        return xhr;
-      },
-      success: function(data) {
-        var img = document.createElement("img");
-        var binaryData = [];
-        binaryData.push(data);
-        img.src = URL.createObjectURL(
-          new Blob(binaryData, { type: "image/png" })
-        );
-        img.width = 797;
-        img.height = 512;
-
-        var canvas = document.createElement("canvas");
-        canvas.width = 600;
-        canvas.height = 400;
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0, 600, 400);
-
-        var view = document.getElementById("view");
-        var dataurl = canvas.toDataURL("image/png");
-        view.src = dataurl;
-      },
-      error: function(xhr) {
-        console.error("error getting image: ", xhr);
-      }
-    });
-  }
-
-  getImage();
-
-  var params = {
-    active: true,
-    currentWindow: true
-  };
-  chrome.tabs.query(params, gotTabs);
-
-  var msg = "msg";
-  function gotTabs(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, msg); //, messageBack);
-  }
 }
 
 $(window).bind("load", function() {
